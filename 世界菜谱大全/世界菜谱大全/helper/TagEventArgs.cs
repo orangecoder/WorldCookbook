fc2e8f772bcb76fef7e0e51d44b59013a33f98f6
李晓平 <619274036@qq.com>
2012-09-12 12:30:09 +0800

    添加项目工程文件
    源代码见世界菜谱大全的文件夹

---
+++ 世界菜谱大全/世界菜谱大全/helper/TagEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace 世界菜谱大全.helper
+{
+    public class TagEventArgs : EventArgs
+    {
+        public TagEventArgs() { }
+
+        public object Tag { get; set; }
+    }
+}
---
+++ 世界菜谱大全/世界菜谱大全/view/MenuDetialViewPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using 世界菜谱大全.userControl;
+
+namespace 世界菜谱大全.view
+{
+    public partial class MenuDetialViewPage : PhoneApplicationPage
+    {
+        public MenuDetialViewPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if(Cache.Instance.nation.menu != null)
+            {
+                this.PageTitle.Text = Cache.Instance.menu.menuName;
+                this.content.DataContext = Cache.Instance.menu;
+            }
+            base.OnNavigatedTo(e);
+        }
+    }
+}
